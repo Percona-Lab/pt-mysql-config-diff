eba1281db4517f1773e3399c64ede4c25700dae1
Carlos Salguero <cfsalguero@gmail.com>
2017-05-11 09:56:47 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,327 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+	flag "github.com/spf13/pflag"
+	ini "gopkg.in/ini.v1"
+)
+
+type options struct {
+	CNFs        []string
+	DSNs        dsnFlags
+	Help        bool
+	compareBase string // First CNF or first MySQL used as comparisson base
+}
+
+type dsnFlag struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Database string
+	Table    string
+	protocol string
+}
+
+type dsnFlags []dsnFlag
+
+func (d dsnFlags) String() string {
+	parts := []string{}
+
+	//if d.Host != "" {
+	//	parts = append(parts, "h="+d.Host)
+	//}
+	//if d.Port != 0 {
+	//	parts = append(parts, fmt.Sprintf("P=%d", d.Port))
+	//}
+	//if d.User != "" {
+	//	parts = append(parts, "u="+d.User)
+	//}
+	//if d.Password != "" {
+	//	parts = append(parts, "p="+d.Password)
+	//}
+	//if d.Database != "" {
+	//	parts = append(parts, "D="+d.Database)
+	//}
+	//if d.Table != "" {
+	//	parts = append(parts, "t="+d.Table)
+	//}
+
+	return strings.Join(parts, ",")
+}
+
+func (d dsnFlags) Set(value string) error {
+	parts := strings.Split(value, ",")
+
+	var dsn dsnFlag
+	for _, part := range parts {
+		if len(part) < 3 {
+			continue
+		}
+		key := string(part[0])
+		value := string(part[2:])
+		switch key {
+		case "D":
+			dsn.Database = value
+		case "h":
+			dsn.Host = value
+		case "p":
+			dsn.Password = value
+		case "P":
+			port, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				dsn.Port = int(port)
+			}
+		case "t":
+			dsn.Table = value
+		case "u":
+			dsn.User = value
+		}
+	}
+
+	if dsn.Host == "localhost" {
+		dsn.protocol = "unix"
+	} else {
+		dsn.protocol = "tcp"
+	}
+	d = append(d, dsn)
+	return nil
+}
+
+func (d dsnFlags) Type() string {
+	return "dsn"
+}
+
+func main() {
+	opts, err := processParams(os.Args[1:])
+	if err != nil {
+		os.Exit(1)
+	}
+
+	// Make a func to connect to the db, so it can be mocked on tests
+	dbConnector := func(dsn string) (*sql.DB, error) {
+		db, err := sql.Open("mysql", dsn)
+		if err != nil {
+			return nil, err
+		}
+		return db, nil
+	}
+
+	configs, err := getConfigs(opts, dbConnector)
+	if err != nil {
+		log.Printf("Cannot get configs: %s", err.Error())
+		os.Exit(1)
+	}
+
+	diffs := compare(configs)
+
+	for key, val := range diffs {
+		fmt.Printf("%35s: %40s : %40s\n", key, val[0], val[1])
+	}
+}
+
+func newCNFReader(filename string) (configReader, error) {
+	cfg, err := ini.LoadSources(ini.LoadOptions{AllowBooleanKeys: true}, filename)
+	if err != nil {
+		return nil, err
+	}
+	if cfg == nil {
+		return nil, fmt.Errorf("Invalid file: %s", filename)
+	}
+
+	cnf := &config{configType: "cnf", entries: make(map[string]interface{})}
+
+	for _, key := range cfg.Section("mysqld").Keys() {
+		cnf.entries[key.Name()] = key.Value()
+	}
+
+	return cnf, nil
+}
+
+func newMySQLReader(db *sql.DB) (configReader, error) {
+	// Since the MySQL driver uses a lazy connection, check if we really can
+	// connect to the db
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	rows, err := db.Query("SHOW VARIABLES")
+	if err != nil {
+		return nil, err
+	}
+
+	ini := &config{configType: "mysql", entries: make(map[string]interface{})}
+
+	for rows.Next() {
+		var key string
+		var val interface{}
+		err := rows.Scan(&key, &val)
+		if err != nil {
+			continue
+		}
+
+		ini.entries[key] = val
+	}
+	return ini, nil
+}
+
+/*
+   We need to compare cfg1 vs cfg2 and cfg2 vs cfg1.
+   Configs can be:
+
+    cfg1      | cfg2
+   -----------+----------
+    key1 = A  | key1 = A
+    key2 = B  | key2 = C
+    key3 = D  |
+              | key4 = E
+
+	So we need 2 inner loops: first through cfg1 keys and then through
+	cfg2 keys to be able to compare the keys that exist in cfg2 but are
+	missing in cfg1.
+
+	MySQL SHOW VARIABLES will return ALL variables but we must skip variables
+	in MySQL config that are missing in the cnf.
+	In the example above, if cfg2 is "cnf" type, key4 must be included in
+	the diff but, if cfg2 type is "mysql", it must be excluded from the diff.
+
+*/
+func compare(configs []configReader) map[string][]interface{} {
+	diffs := make(map[string][]interface{})
+
+	if len(configs) < 2 {
+		return nil
+	}
+	for i := 1; i < len(configs); i++ {
+
+		for key, value1 := range configs[0].Entries() {
+			value2, ok := configs[i].Get(key)
+			if !ok && (configs[0].Type() != "mysql" || configs[0].Type() == configs[1].Type()) {
+				addDiff(diffs, key, value1, "<Missing>")
+				continue
+			}
+
+			// Adjust numbers truncating unnecessary 0s so 10.0 (as string) == 10
+			float1, err := strconv.ParseFloat(fmt.Sprintf("%s", value1), 64)
+			if err == nil {
+				value1 = fmt.Sprintf("%.0f", float1)
+			}
+
+			float2, err := strconv.ParseFloat(fmt.Sprintf("%s", value2), 64)
+			if err == nil {
+				value2 = fmt.Sprintf("%.0f", float2)
+			}
+
+			if fmt.Sprintf("%s", value1) != fmt.Sprintf("%s", value2) {
+				addDiff(diffs, key, value1, value2)
+				continue
+			}
+		}
+
+		for key, value1 := range configs[i].Entries() {
+			_, ok := configs[0].Get(key)
+			if !ok && (configs[i].Type() != "mysql" || configs[0].Type() == configs[i].Type()) {
+				addDiff(diffs, key, "<Missing>", value1)
+			}
+		}
+	}
+
+	return diffs
+}
+
+func addDiff(diffs map[string][]interface{}, key string, value1, value2 interface{}) {
+	if _, ok := diffs[key]; !ok {
+		diffs[key] = append(diffs[key], value1)
+	}
+	diffs[key] = append(diffs[key], value2)
+}
+
+func processParams(arguments []string) (*options, error) {
+	opts := &options{}
+
+	fs := flag.NewFlagSet("default", flag.ContinueOnError)
+	fs.StringArrayVarP(&opts.CNFs, "cnf", "c", nil, "cnf file name")
+	fs.VarP(opts.DSNs, "dsn", "d", "full db dsn. Example: user:pass@tcp(127.1:3306)")
+
+	err := fs.Parse(arguments)
+
+	if err != nil {
+		return nil, err
+	}
+
+	fs.SortFlags = false
+	fs.Visit(func(f *flag.Flag) {
+		if opts.compareBase != "" {
+			return
+		}
+		switch f.Name {
+		case "cnf":
+			opts.compareBase = "cnf"
+		case "dsn":
+			opts.compareBase = "dsn"
+		}
+	})
+
+	return opts, nil
+}
+
+func getConfigs(opts *options, dbConnector func(string) (*sql.DB, error)) ([]configReader, error) {
+	var configs []configReader
+
+	cnfs, err := getCNFs(opts.CNFs)
+	if err != nil {
+		return nil, err
+	}
+
+	mysqls, err := getMySQLs(opts.DSNs, dbConnector)
+	if err != nil {
+		return nil, err
+	}
+
+	if opts.compareBase == "mysql" {
+		configs = append(mysqls, cnfs...)
+	} else {
+		configs = append(cnfs, mysqls...)
+	}
+
+	return configs, nil
+}
+
+func getCNFs(filenames []string) ([]configReader, error) {
+	var configs []configReader
+
+	for _, filename := range filenames {
+		cfg, err := newCNFReader(filename)
+		if err != nil {
+			return nil, fmt.Errorf("Cannot read %s: %s", filename, err.Error())
+		}
+		configs = append(configs, cfg)
+	}
+
+	return configs, nil
+}
+
+func getMySQLs(dsns dsnFlags, dbConnector func(string) (*sql.DB, error)) ([]configReader, error) {
+	var configs []configReader
+
+	for _, dsn := range dsns {
+		db, err := dbConnector(dsn))
+		if err != nil {
+			return nil, fmt.Errorf("Cannot connect to the db %s", err.Error())
+		}
+		cfg, err := newMySQLReader(db)
+		if err != nil {
+			return nil, fmt.Errorf("Cannot read the config variables: %s", err.Error())
+		}
+		configs = append(configs, cfg)
+	}
+
+	return configs, nil
+}
---
+++ main_test.go
@@ -0,0 +1,246 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"reflect"
+	"testing"
+
+	sqlmock "gopkg.in/DATA-DOG/go-sqlmock.v1"
+)
+
+func TestCompareCNFs(t *testing.T) {
+
+	mockConfig1 := &config{
+		configType: "cnf",
+		entries: map[string]interface{}{
+			"key1": "value1",
+			"key2": 2,
+			"key3": true,
+		},
+	}
+
+	mockConfig2 := &config{
+		configType: "cnf",
+		entries: map[string]interface{}{
+			"key1": "value1",
+			"key2": 3,
+			"key4": true,
+		},
+	}
+
+	want := map[string][]interface{}{
+		"key2": []interface{}{2, 3},
+		"key3": []interface{}{true, "<Missing>"},
+		"key4": []interface{}{"<Missing>", true},
+	}
+
+	got := compare([]configReader{mockConfig1, mockConfig2})
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got:\n%#v\nWant:\n %#v\n", got, want)
+	}
+
+}
+
+func TestCompareCNFvsMySQL(t *testing.T) {
+
+	mockConfig1 := &config{
+		configType: "cnf",
+		entries: map[string]interface{}{
+			"key1": "value1",
+			"key2": 2,
+			"key3": true,
+		},
+	}
+
+	// MySQL SHOW VARIABLES will return ALL variables but we must skip variables in
+	// MySQL config that are missing in the cnf.
+	// In this particular case, key4 should not be included in the diff
+	mockConfig2 := &config{
+		configType: "mysql",
+		entries: map[string]interface{}{
+			"key1": "value1",
+			"key2": 3,
+			"key4": true,
+		},
+	}
+
+	want := map[string][]interface{}{
+		"key2": []interface{}{2, 3},
+		"key3": []interface{}{true, "<Missing>"},
+	}
+
+	got := compare([]configReader{mockConfig1, mockConfig2})
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got:\n%#v\nWant:\n %#v\n", got, want)
+	}
+
+}
+
+func TestAddDiff(t *testing.T) {
+
+	diffs := make(map[string][]interface{})
+
+	want1 := map[string][]interface{}{"key1": []interface{}{"value1", "value2"}}
+	addDiff(diffs, "key1", "value1", "value2")
+	if !reflect.DeepEqual(diffs, want1) {
+		t.Errorf("Error adding key/val: Got\n%#v, want\n%#v\n", diffs, want1)
+	}
+
+	want2 := map[string][]interface{}{"key1": []interface{}{"value1", "value2", "value3"}}
+	addDiff(diffs, "key1", "value1", "value3")
+	if !reflect.DeepEqual(diffs, want2) {
+		t.Errorf("Error adding key/val: Got\n%#v, want\n%#v\n", diffs, want2)
+	}
+
+}
+
+func TestReadCNFs(t *testing.T) {
+
+	cnf, err := newCNFReader("some_fake_file")
+	if err == nil {
+		t.Error("Should return error on invalid files")
+	}
+
+	want := &config{
+		configType: "cnf",
+		entries: map[string]interface{}{
+			"sql_mode":                       "IGNORE_SPACE,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION",
+			"innodb_buffer_pool_size":        "512M",
+			"log_slow_rate_limit":            "100.1234",
+			"log_slow_verbosity":             "full",
+			"basedir":                        "/usr",
+			"innodb_flush_log_at_trx_commit": "2",
+			"log_slow_rate_type":             "query",
+			"log_slow_admin_statements":      "ON",
+			"pid-file":                       "/var/run/mysqld/mysqld.pid",
+			"socket":                         "/var/run/mysqld/mysqld.sock",
+			"bind-address":                   "127.0.0.1",
+			"slow_query_log":                 "OFF",
+			"user":                           "mysql",
+			"log_slow_slave_statements":         "ON",
+			"datadir":                           "/var/lib/mysql",
+			"local-infile":                      "1",
+			"explicit_defaults_for_timestamp":   "true",
+			"secure-file-priv":                  "\"\"",
+			"log-error":                         "/var/log/mysql/error.log",
+			"log_output":                        "file",
+			"slow_query_log_use_global_control": "all",
+			"tmpdir":                           "/tmp",
+			"lc-messages-dir":                  "/usr/share/mysql",
+			"long_query_time":                  "0",
+			"port":                             "3306",
+			"max_allowed_packet":               "128M",
+			"symbolic-links":                   "0",
+			"key_buffer_size":                  "512M",
+			"slow_query_log_file":              "/var/log/mysql/slow.log",
+			"slow_query_log_always_write_time": "1",
+		},
+	}
+
+	cnf, err = newCNFReader("./test/mysqld.cnf")
+	if err != nil {
+		t.Errorf("Shouldn't return error on existent file: %s", err.Error())
+	}
+
+	if !reflect.DeepEqual(cnf, want) {
+		fmt.Printf("Got:\n%#v\nWant: %#v\n", cnf, want)
+	}
+
+}
+
+func TestReadMySQL(t *testing.T) {
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	columns := []string{"Variable_name", "Value"}
+
+	mock.ExpectQuery("SHOW VARIABLES").WillReturnRows(sqlmock.NewRows(columns).
+		AddRow("innodb_buffer_pool_size", "512M").
+		AddRow("log_slow_rate_limit", "100.1234").
+		AddRow("log_slow_verbosity", "full"))
+
+	want := &config{
+		configType: "mysql",
+		entries: map[string]interface{}{
+			"innodb_buffer_pool_size": "512M",
+			"log_slow_rate_limit":     "100.1234",
+			"log_slow_verbosity":      "full",
+		},
+	}
+
+	cnf, err := newMySQLReader(db)
+	if err != nil {
+		t.Errorf("Shouldn't return error on mock up db: %s", err.Error())
+	}
+
+	if !reflect.DeepEqual(cnf, want) {
+		fmt.Printf("Got:\n%#v\nWant: %#v\n", cnf, want)
+	}
+
+}
+
+func TestGetConfigs(t *testing.T) {
+
+	opts := &options{
+		CNFs: []string{"./test/mysqld.cnf"},
+		DSNs: []string{"mock:pass@tcp(127.1:3306)/"},
+	}
+
+	mockDBConnector := func(dns string) (*sql.DB, error) {
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		}
+
+		columns := []string{"Variable_name", "Value"}
+
+		mock.ExpectQuery("SHOW VARIABLES").WillReturnRows(sqlmock.NewRows(columns).
+			AddRow("innodb_buffer_pool_size", "512M").
+			AddRow("log_slow_rate_limit", "100.1234").
+			AddRow("log_slow_verbosity", "full"))
+
+		return db, nil
+	}
+
+	configs, err := getConfigs(opts, mockDBConnector)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(configs) != 2 {
+		t.Errorf("There must be 2 configs, got %d", len(configs))
+	}
+
+	if configs[0].Type() != "cnf" {
+		t.Errorf("First config should be a cnf file. Got: %s", configs[0].Type())
+	}
+
+	if configs[1].Type() != "mysql" {
+		t.Errorf("Second config should be 'mysql'. Got: %s", configs[1].Type())
+	}
+
+}
+
+func TestProcessParams(t *testing.T) {
+	args := []string{"--dsn=h=127.1,P=12345,u=user1,p=pass,D=db,t=table", "--cnf=mysqld.conf"}
+	opts, err := processParams(args)
+	if err != nil {
+		t.Errorf("Cannot parse params")
+	}
+	if opts.compareBase != "dsn" {
+		t.Errorf("Compare base must be dsn. Got %s", opts.compareBase)
+	}
+
+	args = []string{"--cnf=mysqld.conf", "--dsn=h=127.1,P=12345,u=user1,p=pass,D=db,t=table"}
+	opts, err = processParams(args)
+	if opts.compareBase != "cnf" {
+		t.Errorf("Compare base must be cnf. Got %s", opts.compareBase)
+	}
+}
